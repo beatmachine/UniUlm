6567e521a04918ce9fdd71a8cfe1c9c4234320be
Tim Mend <tim.mend@uni-ulm.de>
2017-04-21 00:35:17 +0200

    Excercise for UIST
    Project +Excersice for UIST

---
+++ UIST/VisualStudio/CSharpChatBot/CSharpChatBot/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpChatBot
+{
+    class Program
+
+    { 
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            
+
+            Dictionary<String, String> dic = new Dictionary<string, string>();
+            dic.Add("TELL_DATE", String.Format("Todays Date is {0}", DateTime.Now));
+            dic.Add("TELL_JOKE", "Why can't Java Developers wear glasses? Because they don't C#!");
+            dic.Add("SAY_HI", String.Format("Hello {0}, nice too meet you!", name));
+
+            Console.WriteLine("Enter a command!");
+            foreach(KeyValuePair<String, String> pair in dic)
+            {
+                Console.WriteLine(pair.Key);
+            }
+            var input = "";
+            while ((input = Console.ReadLine()) != String.Empty)
+            {
+                
+                if (dic.ContainsKey(input))
+                {
+                    Console.WriteLine(dic[input]);
+                }
+                else
+                {
+                    Console.WriteLine("THIS ISN'T A LEGIT COMMAND");
+                }
+
+            }
+            {
+
+            }
+
+        }
+    }
+}
---
+++ UIST/VisualStudio/Excercise1/Excercise1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Excercise1
+{
+    public enum Language
+    {
+        CSHARP,
+        FUCKYOU,
+        BITCH = 5000
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {   //Initiating some Lists
+            int[] arr2 = { 1, 2, 3, 4, 5 };
+            List<string> stringList = new List<string>();
+            stringList.Add("Fuckyou");
+            stringList.Add("HAHAHAHAHA");
+            stringList.Add("BLAALÖSKDöl");
+            //Iterate the Items
+            IEnumerator enumartor = stringList.GetEnumerator();
+            Dictionary<int, object> somedic = new Dictionary<int, object>();
+            somedic.Add(1234, "niggnogg");
+            somedic.Add(2222, new DateTime());
+
+            while(enumartor.MoveNext())
+            {
+                Console.WriteLine("List item : {0}", enumartor.Current.ToString());
+            }
+
+            foreach(var i in arr2)
+            {
+                Console.Write("This is the number {0}\n", i);
+                 
+            }
+            foreach(KeyValuePair<int, object> s in somedic)
+            {
+                Console.Write("Here you {0} go {1}\n", s.Key, s.Value.GetType());
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
